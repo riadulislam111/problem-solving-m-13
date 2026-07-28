40d45623701ed3745578cc49cef5ea8fabe8fae1
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-28 07:06:53 +0600

    add all fils code lines and updats

---
+++ problem-19.js
@@ -0,0 +1,41 @@
+// // Input: an object (values are unique)
+// // Output: a new object with keys and values swapped
+// // Returns: an object
+ 
+// function invertObject(obj) {
+//   let inverted = {};
+//   // TODO: loop through obj and swap each key/value pair into inverted
+ 
+// for(const [key , value] of Object.entries(obj)){
+//     inverted[value] = key;
+// }
+
+
+//   return inverted;
+// }
+ 
+// console.log(invertObject({ a: 1, b: 2, c: 3 }));
+// // Expected: { 1: "a", 2: "b", 3: "c" }
+
+
+
+
+// use map 
+
+
+// Input: an object (values are unique)
+// Output: a new object with keys and values swapped
+// Returns: an object
+ 
+function invertObject(obj) {
+  let inverted = {};
+  // TODO: loop through obj and swap each key/value pair into inverted
+ 
+const 
+
+
+  return inverted;
+}
+ 
+console.log(invertObject({ a: 1, b: 2, c: 3 }));
+// Expected: { 1: "a", 2: "b", 3: "c" }
